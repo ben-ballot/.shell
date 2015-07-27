0d9ce6fdab37979f55e2e30f2905d5216d06b41f
Benjamin Allot <darkalia@users.noreply.github.com>
2015-07-27 10:26:45 +0200

    Add https://github.com/darkalia/git-aware-prompt stuff for git repositories

---
+++ lib/Linux/default/99-prompt.sh
@@ -0,0 +1,5 @@
+if [ "$color_prompt" = yes ]; then
+	export PS1="\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+else
+	export PS1="\${debian_chroot:+(\$debian_chroot)}\u@\h:\w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+fi
