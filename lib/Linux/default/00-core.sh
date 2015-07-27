feaa3fc51378be550ed6f1185c651740aa77514b
Benjamin Allot <darkalia@users.noreply.github.com>
2015-07-27 10:33:39 +0200

    Last version of _pwait

--- lib/Linux/default/00-core.sh
+++ lib/Linux/default/00-core.sh
@@ -1,17 +1,18 @@
-_pwait () {
-        local _pids=$@ _timeout=10 _pid _cmdpid
-
-        # No pwait equivalent on Linux. We wait for all processes to finish
-        #  until $_timeout exprires
-        if [ "x${_pids}" != "x" ];then
-                ( for _pid in "${_pids}"
-                do
-                        ( while [ -d /proc/${_pid} ]; do sleep 0.5 ; done ) &
-                done
-                ) & _cmdpid=$!
-                ( sleep ${_timeout} ; echo "Killing group pid $_cmdpid" ; kill -- -${_cmdpid} ) &
-		echo "Waiting pid $_cmdpid"
-                wait ${_cmdpid}
-
-        fi
+_pwait () { 
+        # We wait for all processes to finish until $_timeout expires 
+        local _pids=$@ _timeout=10 _pid _cmdpid 
+ 
+        # Create a subshell which will test the processes and get the pid of 
+        #  said subshell 
+        ( for _pid in ${_pids} 
+        do 
+                # Check the existence of the directory related to the 
+                #  the pid in proc fs each second 
+                ( while [ -d /proc/${_pid} ]; do sleep 1 ; done ) &  
+        done 
+        ) & _cmdpid=$! 
+        # in another subshell, wait $_timeout then kill the subshell checking 
+        #  the existence of PIDs, avoiding being stuck checking 
+        ( sleep ${_timeout} ; kill -- -${_cmdpid} 2>/dev/null ) & 
+        wait ${_cmdpid} 
 }
