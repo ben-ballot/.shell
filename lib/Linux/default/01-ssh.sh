30bfe866651db06e70cf4d411efc9b5939330bce
Benjamin Allot <darkalia@users.noreply.github.com>
2016-04-11 09:28:00 +0200

    Removed LXC dummy test

--- lib/Linux/default/01-ssh.sh
+++ lib/Linux/default/01-ssh.sh
@@ -6,7 +6,7 @@ function start_agent {
      echo succeeded
      chmod 600 "${SSH_ENV}"
      . "${SSH_ENV}" > /dev/null
-     for ssh_key in "${HOME}/.ssh/*dsa*" "${HOME}/.ssh/*rsa*"
+     for ssh_key in "${HOME}/.ssh/*dsa*"
      do
          /usr/bin/ssh-add ${ssh_key%*.pub}
      done
@@ -14,14 +14,12 @@ function start_agent {
 
 # Source SSH settings, if applicable
 
-if [ -f "${SSH_ENV}" -a ! grep /lxc /proc/1/cgroup >/dev/null 2>&1 ]; then
+if [ -f "${SSH_ENV}" ]; then
      . "${SSH_ENV}" > /dev/null
      #ps ${SSH_AGENT_PID} doesn't work under cywgin
-     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent > /dev/null || {
          start_agent
      }
-else
-     if ! pgrep gnome-keyring >/dev/null 2>&1; then
+elif ! pgrep gnome-keyring >/dev/null 2>&1; then
          start_agent
-     fi
 fi
