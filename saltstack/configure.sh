56e9684bcbda99072dce894583a9055e7949785d
Benjamin Allot <darkalia@users.noreply.github.com>
2016-11-17 16:38:30 +0100

    salt: Wrapper around salt-call --local

---
+++ saltstack/configure.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+SCRIPT="$(readlink -f "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT")"
+
+[ -x "$(which salt-call)" ]
+
+sudo salt-call --local \
+    --file-root="$SCRIPT_DIR"/salt \
+    --pillar-root="$SCRIPT_DIR"/pillar \
+    --retcode-passthrough \
+    -l quiet \
+    state.apply \
+    "$@"
