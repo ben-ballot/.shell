fbb1df2bc7a6478560282d1e5f290181e23edecd
Benjamin Allot <darkalia@users.noreply.github.com>
2017-04-24 11:22:19 +0200

    add opentsack push_image function

---
+++ lib/Linux/default/99-openstack.sh
@@ -0,0 +1,14 @@
+openstack_push_image() {
+    local -a args
+    local name="$1"
+
+    args+=("--disk-format" "qcow2")
+    args+=("--min-disk" "5")
+    args+=("--min-ram" "256")
+    args+=("--container-format" "bare")
+    args+=("--property" "img_config_drive=mandatory")
+    args+=("--file" "$2")
+
+    echo openstack image create "$name" "${args[@]}"
+    openstack image create "$name" "${args[@]}"
+}
