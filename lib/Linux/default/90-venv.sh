98659527a215c1e27d3528ade4ac1fa80d616c94
Benjamin Allot <benjamin.allot@canonical.com>
2022-10-12 00:57:05 +0200

    Source virtualenvwrapper

--- lib/Linux/default/90-venv.sh
+++ lib/Linux/default/90-venv.sh
@@ -1,4 +1,5 @@
 if [ -d "$HOME/.virtualenvs" ];then
     export WORKON_HOME="$HOME/.virtualenvs"
     alias venv="source /usr/share/virtualenvwrapper/virtualenvwrapper.sh"
+    source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 fi
