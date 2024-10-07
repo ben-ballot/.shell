3e6c9ab1f103d619e18038846f8a2c2b4802f9cf
Benjamin Allot <benjamin.allot@canonical.com>
2024-10-08 01:20:10 +0200

    Add kubeconfig switch functions

--- lib/Linux/default/99-kubernetes.sh
+++ lib/Linux/default/99-kubernetes.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 # Enable completion if kubectl is found
+
 # shellcheck disable=SC2039
 if which kubectl &> /dev/null; then
     # shellcheck disable=SC2039 disable=SC1090
@@ -10,3 +12,33 @@ if which helm &> /dev/null; then
     # shellcheck disable=SC2039 disable=SC1090
     . <(helm completion bash)
 fi
+
+# add to your shell startup script (e.g: $HOME/.bashrc).
+# install [fzf](https://github.com/junegunn/fzf)
+
+kctx() {
+    local kubeconfig_dir="${HOME}/.kube"
+    if ! [ -d "${kubeconfig_dir}" ]; then
+        echo "No ${kubeconfig_dir} exists"
+        return
+    fi
+    local SEARCH="${1:-${HOME}/.kube}"
+    local KUBE
+    KUBE="$(find "${SEARCH}" -type f -name "*.kubeconfig" | fzf)"
+    [[ -n "$KUBE" ]] && ln -sf "$KUBE" "${kubeconfig_dir}/config"
+}
+
+klctx() {
+    local kubeconfig_dir="${HOME}/.kube"
+    local SEARCH="${1:-${kubeconfig_dir}}"
+    if ! [ -d "${kubeconfig_dir}" ]; then
+        echo "No ${kubeconfig_dir} exists"
+        return
+    fi
+    local KUBE
+    KUBE="$(find "${SEARCH}" -type f -name "*.kubeconfig" | fzf)"
+    if [[ -n "$KUBE" ]]; then
+      #export KUBECONFIG="${SEARCH%/}/$KUBE"
+      export KUBECONFIG="$KUBE"
+    fi
+}
