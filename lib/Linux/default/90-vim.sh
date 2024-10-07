22dc0b57e992ce2bc3394358b005728048c96964
Benjamin Allot <benjamin.allot@canonical.com>
2024-10-08 01:18:38 +0200

    Store vim sessions

--- lib/Linux/default/90-vim.sh
+++ lib/Linux/default/90-vim.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
 vs() {
     local servername
     local editor
+    local session_dir
+    local vim_args=()
 
+    session_dir="${HOME}/.vim/sessions"
+    if [[ ! -d "${HOME}" ]]; then
+        mkdir -p "${session_dir}"
+    fi
     if [ "$1" = "--editor" ]; then
         editor=$2
         shift 2
@@ -12,18 +20,31 @@ vs() {
         servername="vimserver"
     else
         servername="$VS"
+        session_file="${session_dir}/${servername}.vim"
+        if [[ -f "${session_file}" ]]; then
+            vim_args+=(-S "${session_file}")
+        fi
+        vim_args+=(--servername "${servername}" -c "Obsess ${session_file}")
     fi
-    if [ $# -eq 0 ]; then
-        "$editor" --servername "$servername"
-    elif [ $# -eq 1 ]; then
-        "$editor" --servername "$servername" --remote-silent "$@"
-    else
-        "$editor" -O2 --servername "$servername" --remote-silent "$@"
+    vim_args+=(--servername "${servername}")
+
+    if [ $# -eq 1 ]; then
+        vim_args+=(--remote-silent)
+    elif [ $# -gt 1 ]; then
+        vim_args+=(-O2)
     fi
+    "${editor}" "${vim_args[@]}" "$@"
 }
 
 gvs() {
     vs --editor gvim "$@"
+}
+
+setvs() {
+    local curdir
+    curdir="$(basename ${PWD})"
+
+    export VS="${curdir}"
 }
 
 # vim: ft=sh
